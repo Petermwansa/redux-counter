78539c400a70b48fc8deaedae1c2d683631874bf
Petermwansa <mwansapeter800@gmail.com>
2024-06-26 07:52:14 +0300

    Implement the use of different state slices in redux and apply them

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../store';
 
 import classes from './Counter.module.css';
 
@@ -8,25 +9,33 @@ const Counter = () => {
 
   // we use the useselector to retrive a part of the overrall obj 
   //redux will automatically set up a subscription to the store through this comp when you use useSelector
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector(state => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter)
 
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment'})
+    dispatch(counterActions.increment())
+  }
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10))  //the tookit generates a unique identifier, then the input value as arg will be stored in an extra field named payload: 10 
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement'})
+    dispatch(counterActions.decrement())
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>-</button>
+        <button onClick={increaseHandler}>Add 10</button>
         <button onClick={incrementHandler}>+</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -34,4 +43,52 @@ const Counter = () => {
   );
 };
 
+
 export default Counter;
+
+
+// // here we work with class based comps 
+// class Counter extends Component {
+
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement()
+//   }
+
+//   toggleCounterHandler() {
+
+//   }
+
+//   render () {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.decrementHandler.bind()}>-</button>
+//           <button onClick={this.incrementHandler.bind()}>+</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler.bind()}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+// // this maps the redux state to props 
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment'}),
+//     decrement: () => dispatch({ type: 'decrement'}),
+//   }
+// }
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,57 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'; 
 
+const initialCounterState = { counter: 0, showCounter: true};
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
     }
+});
 
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
 
-    return state;
+const initialAuthState = {
+    isAuthenticated: false
 }
 
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state){
+            state.isAuthenticated = false
+        }
+    }
 
-const store = createStore(counterReducer);
+})
+
+
+const store = configureStore({
+    reducer: { 
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
+});
+
+
+// tookit creates action obj for us which we will use to dispatch the actions 
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 // we connect the app to the store by exporting the store 
 export default store;
